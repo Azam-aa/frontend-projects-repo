87664188d323fa7e90f31c1d5044e6526c9da62e
Azam-aa <azamp442@gmail.com>
2025-12-22 17:18:43 +0530

    updated code

--- signup-page/src/Component/Page.jsx
+++ signup-page/src/Component/Page.jsx
@@ -1,17 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Page = () => {
+const [fromData, setFormData] = useState({
+  fullName:"",
+  email:"",
+  password:"",
+  confirmPassword:"",
+})
+
+const [error, setErrors] = useState({});
+const [loading, setLoading] = useState(false);
+const [showPassword, setShowPassword] = useState(false);
+
+const handleChange = (e) => {
+  setFormData({...fromData, [e.target.name]:e.target.value});
+}
+
+const validate = () => {
+  let newErros = {};
+
+  if(FormData.fullName.length < 3){
+    newErrors.fullName = "Name must be at least characters";
+  }
+
+  if(!FormData.email.includes("@")){
+    newErrors.email = "Enter a valid email";
+  }
+
+  if(FormData.password.length < 8){ 
+    newErrors.password = "Password must be character";
+  }
+
+  if(FormData.password !== FormData.confirmPassword){
+    newErrors.confirmPassword = "Password do not match";
+  }
+
+
+  return newErrors;
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const validationErrors = validate();
+
+  if(objectDirection.keys(validationErrors).length > 0){
+    setErrors(validationErrors);
+    return;
+  }
+
+  setErrors({});
+  setLoading(true);
+
+  setTimeout(()=>{
+    console.log("User Registereed:",formData);
+    setLoading(false);
+    alert("Singup successful!");
+  }, 1500);
+
+}
+
   return (
     <div className='mx-3 mt-1 font-[Poppins] rounded-[12px] p-8  '>
       <h1 className='font-bold text-[59.25px] '>Sign up</h1>
       <h3 className='text-[20px]'>Let the awesome begin</h3>
 
-        <form className="mt-14 flex flex-col gap-3">
+        <form onSubmit={handleSubmit} className="mt-14 flex flex-col gap-3">
        
-        <input className='border border-black rounded-[8px] p-4 w-90 ' type="text" placeholder='Full Name'/>
-        <input className='border border-black rounded-[8px] p-4 w-90 ' type="email" placeholder='Email'/>
-        <input className='border border-black rounded-[8px] p-4 w-90 ' type="password" placeholder='Password' />
-        <input className='border border-black rounded-[8px] p-4 w-90 ' type="password" placeholder='Repeat Password'  />
+        <input value={formData.fullName} onChange={handleChange} className='border border-black rounded-[8px] p-4 w-90 ' type="text" placeholder='Full Name'/>
+        <input value={formData.email} onChange={handleChange} className='border border-black rounded-[8px] p-4 w-90 ' type="email" placeholder='Email'/>
+        <input value={formData.password} onChange={handleChange} className='border border-black rounded-[8px] p-4 w-90 ' type="password" placeholder='Password' />
+        <input value={formData.confirmPassword} onChange={handleChange}  className='border border-black rounded-[8px] p-4 w-90 ' type="password" placeholder='Repeat Password'  />
       
 
         <div className='flex flex-row gap-3 mt-5'>
